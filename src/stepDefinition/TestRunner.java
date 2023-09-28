b135ccab651f351820f0d9cf9968b403fe89fcfe
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-28 09:22:46 +0530

    Update TestRunner.java

--- src/stepDefinition/TestRunner.java
+++ src/stepDefinition/TestRunner.java
@@ -42,6 +42,7 @@ public class TestRunner {
 	}
 
 	@Then("^User should be able to login$")
+	
 	public void user_should_be_able_to_login() throws Throwable {
 	driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();  
 	}
