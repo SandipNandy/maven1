3704e0dbc5f9b71761b3f7b87b0d69fec9805abc
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-16 10:01:13 +0530

    Update Testrunner.java

--- src/runner/Testrunner.java
+++ src/runner/Testrunner.java
@@ -2,7 +2,6 @@ package runner;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
 @RunWith(Cucumber.class)
   
 @CucumberOptions(features="feature",glue={"stepDefinition"})
